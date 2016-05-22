1beec03779401bece39c89d433acf2cf9f59c7e0
Rodrigo Dechen <mestri.rodrigo@gmail.com>
2016-05-21 22:36:32 -0300

    Update rdscrollspy.js

--- rdscrollspy.js
+++ rdscrollspy.js
@@ -18,6 +18,7 @@
     });
 
     $(w).scroll(function(){
+
         if(base === null) return;
         var t = $(base);
         var tela = {};
@@ -26,8 +27,8 @@
             tela.bot = tela.top + t.height();
             $(list).each(function (i, e){
 
-                var self = $(e.element);
-                var o = e.options;
+                var self = $(e);
+                var o = e.rdscrollspy;
 
                 var eu = {};
                 eu.top = self.offset().top;
@@ -55,51 +56,53 @@
                 var vis_atu = (self.hasClass(o['class.visible']) ? o['class.visible'] : o['class.invisible']);
 
                 if(vis_ant == o['class.invisible'] && vis_atu == o['class.visible'])
-                    o['visible'].call(e.element, o);
+                    o['visible'].call(e, o);
 
                 if(vis_ant == o['class.invisible'] && vis_atu == o['class.visible'] && pos_atu == o['class.up'])
-                    o['visible.up'].call(e.element, o);
+                    o['visible.up'].call(e, o);
 
                 if(vis_ant == o['class.invisible'] && vis_atu == o['class.visible'] && pos_atu == o['class.down'])
-                    o['visible.down'].call(e.element, o);
+                    o['visible.down'].call(e, o);
 
 
                 if(vis_ant == o['class.visible'] && vis_atu == o['class.invisible'])
-                    o['invisible'].call(e.element, o);
+                    o['invisible'].call(e, o);
 
                 if(vis_ant == o['class.visible'] && vis_atu == o['class.invisible'] && pos_atu == o['class.up'])
-                    o['invisible.up'].call(e.element, o);
+                    o['invisible.up'].call(e, o);
 
                 if(vis_ant == o['class.visible'] && vis_atu == o['class.invisible'] && pos_atu == o['class.down'])
-                    o['invisible.down'].call(e.element, o);
+                    o['invisible.down'].call(e, o);
 
 
                 if(pos_ant != o['class.middle'] && pos_atu == o['class.middle'])
-                    o['inserted'].call(e.element, o);
+                    o['inserted'].call(e, o);
 
                 if(pos_ant == o['class.up'] && pos_atu == o['class.middle'])
-                    o['inserted.up'].call(e.element, o);
+                    o['inserted.up'].call(e, o);
 
                 if(pos_ant == o['class.down'] && pos_atu == o['class.middle'])
-                    o['inserted.down'].call(e.element, o);
+                    o['inserted.down'].call(e, o);
 
 
                 if(pos_ant == o['class.middle'] && pos_atu != o['class.middle'])
-                    o['leave'].call(e.element, o);
+                    o['leave'].call(e, o);
 
                 if(pos_ant == o['class.middle'] && pos_atu == o['class.up'])
-                    o['leave.up'].call(e.element, o);
+                    o['leave.up'].call(e, o);
 
                 if(pos_ant == o['class.middle'] && pos_atu == o['class.down'])
-                    o['leave.down'].call(e.element, o);
+                    o['leave.down'].call(e, o);
 
             });
         }
     });
 
     $.fn.rdscrollspy = function(options, valeu){
-        if(typeof options == 'object' || typeof options == 'undefined')
-            return $(this).each(function(i, e){
+
+        if(typeof options == 'object' || typeof options == 'undefined'){
+
+            return $(this).each(function (i, e) {
                 var opt = $.extend({
                     'offset.top': 0,
                     'class.up': 'ss-up',
@@ -107,28 +110,28 @@
                     'class.middle': 'ss-middle',
                     'class.visible': 'ss-visible',
                     'class.invisible': 'ss-invisible',
-                    'visible': function(){},
-                    'visible.up': function(){},
-                    'visible.down': function(){},
-                    'invisible': function(){},
-                    'invisible.up': function(){},
-                    'invisible.down': function(){},
-                    'inserted': function(){},
-                    'inserted.up': function(){},
-                    'inserted.down': function(){},
-                    'leave': function(){},
-                    'leave.up': function(){},
-                    'leave.down': function(){}
-                }, (typeof e.rdscrollspy !== 'undefined'? e.rdscrollspy: {}), options);
+                    'visible': function () {},
+                    'visible.up': function () {},
+                    'visible.down': function () {},
+                    'invisible': function () {},
+                    'invisible.up': function () {},
+                    'invisible.down': function () {},
+                    'inserted': function () {},
+                    'inserted.up': function () {},
+                    'inserted.down': function () {},
+                    'leave': function () {},
+                    'leave.up': function () {},
+                    'leave.down': function () {}
+                }, (typeof e.rdscrollspy !== 'undefined' ? e.rdscrollspy : {}), options);
 
-                if(typeof e.rdscrollspy !== 'undefined'){
+                if (typeof e.rdscrollspy === 'undefined') {
                     e.rdscrollspy = opt;
                     list.push(e);
-                }else
+                } else
                     e.rdscrollspy = opt;
             });
-        
-        else if(typeof options == 'string' && typeof valeu == 'undefined'){
+
+        }else if(typeof options == 'string' && typeof valeu == 'undefined'){
             var i = $(list).index($(this).eq(0));
             if(i >= 0) return list[i].rdscrollspy[options];
 
@@ -136,7 +139,7 @@
             var i = $(list).index($(this).eq(0));
             if(i >= 0) list[i].rdscrollspy[options] = valeu;
             return this;}
-        
+
         return null;
     };
 
